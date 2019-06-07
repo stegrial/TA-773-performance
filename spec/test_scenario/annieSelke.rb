73883dac4bd8f507724ca19b0ea2b8af40b2f30f
jarapova <arapova.jul2@gmail.com>
2019-06-07 15:19:50 +0300

    AS_fix for new version

--- spec/test_scenario/annieSelke.rb
+++ spec/test_scenario/annieSelke.rb
@@ -1,56 +1,184 @@
 
 require 'spec_helper'
 feature 'TrueAutomation.IO capybara example' do
-  scenario 'Annie Selke - Create account' do
+
+  scenario 'Recording - Create account' do
+
+    # User goes to the main page of the site Annieselke
     visit 'https://annieselke.com/'
 
-    sleep 5
-    within_frame(1) do
-        page.execute_script("document.querySelector('button.close').click()")
+      # User clicks on close button in offer modal
+      within_frame(1) do
+            page.execute_script("document.querySelector('button.close').click()")
+      end
+      # User clicks on login button in main page
+      find(:xpath, ta('AS:mainPage:loginDesktop', "//a[text()='Login']")).click
+      # User clicks on create account for starting greating new account
+      find(:xpath, ta('AS:mainPage:createAccount', "//h3[text()='Create an Account']")).click
+      # User set first name in matching input line
+      find(:xpath, ta('AS:loginPage:firstName', "//input[@id='register.firstName']")).set('youre.example@gmail.com')
+      # User set last name in matching input line
+      find(:xpath, ta('AS:loginPage:lastName', "//input[@id='register.lastName']")).set('name')
+      # User set email in matching input line
+      find(:xpath, ta('AS:loginPage:email', "//input[@id='register.email']")).set('name')
+      # User set pass in matching input line
+      find(:xpath, ta('AS:loginPage:password', "//input[@id='password']")).set('1234tesT')
+      # User repeat pass name in matching input line
+      find(:xpath, ta('AS:loginPage:checkPwd', "//input[@id='register.checkPwd']")).set('1234tesT')
+      # User click in checkbox for subscribe
+      find(:xpath, ta('AS:loginPage:subscribe', "//label[@for='subscribe']")).click
+      # User click register button to complete the registration
+      find(:xpath, ta('AS:loginPage:register', "(//button[@class='primary actionSet'])[2]")).click
+
+      sleep 3
     end
-    sleep 5
-    start_step_1 = Time.now
 
-    1.times do
+    #we need to break the session, as the site uses frames
+    scenario 'Searching - Create account' do
+      1.times do
 
-      find(:xpath, ta('performanse:AS:mainPage:loginDesktop', "//a[text()='Login']")).click
-      finish_step_1 = Time.now
-      puts time_step_1 = "time of step 1, seconds: #{(finish_step_1 - start_step_1).to_i}"
+        visit 'https://annieselke.com'
+        within_frame(1) do
+              page.execute_script("document.querySelector('button.close').click()")
+        end
 
-      find(:xpath, ta('performanse:AS:mainPage:createAccount', "//h3[text()='Create an Account']")).click
-      finish_step_2 = Time.now
-      puts time_step_2 = "time of step 2, seconds: #{(finish_step_2 - finish_step_1).to_i}"
+        sleep 5
+        start_step_1 = Time.now
 
-      find(:xpath, ta('performanse:AS:loginPage:firstName', "//input[@id='register.firstName']")).set('youre.example@gmail.com')
-      finish_step_3 = Time.now
-      puts time_step_3 = "time of step 3, seconds: #{(finish_step_3 - finish_step_2).to_i}"
+        find(:xpath, ta('AS:mainPage:loginDesktop', "//a[text()='Login']")).click
+        finish_step_1 = Time.now
+        puts time_step_1 = "time of step 1, seconds: #{(finish_step_1 - start_step_1).to_i}"
 
-      find(:xpath, ta('performanse:AS:loginPage:lastName', "//input[@id='register.lastName']")).set('name')
-      finish_step_4 = Time.now
-      puts time_step_4 = "time of step 4, seconds: #{(finish_step_4 - finish_step_3).to_i}"
+        sleep 5
+        start_step_2 = Time.now
 
-      find(:xpath, ta('performanse:AS:loginPage:email', "//input[@id='register.email']")).set('name')
-      finish_step_5 = Time.now
-      puts time_step_5 = "time of step 5, seconds: #{(finish_step_5 - finish_step_4).to_i}"
+        find(:xpath, ta('AS:mainPage:createAccount', "//h3[text()='Create an Account']")).click
+        finish_step_2 = Time.now
+        puts time_step_2 = "time of step 2, seconds: #{(finish_step_2 - start_step_2).to_i}"
 
-      find(:xpath, ta('performanse:AS:loginPage:password', "//input[@id='password']")).set('1234tesT')
-      finish_step_6 = Time.now
-      puts time_step_6 = "time of step 6, seconds: #{(finish_step_6 - finish_step_5).to_i}"
+        sleep 5
+        start_step_3 = Time.now
 
-      find(:xpath, ta('performanse:AS:loginPage:checkPwd', "//input[@id='register.checkPwd']")).set('1234tesT')
-      finish_step_7 = Time.now
-      puts time_step_7 = "time of step 7, seconds: #{(finish_step_7 - finish_step_6).to_i}"
+        find(:xpath, ta('AS:loginPage:firstName', "//input[@id='register.firstName']")).set('youre.example@gmail.com')
+        finish_step_3 = Time.now
+        puts time_step_3 = "time of step 3, seconds: #{(finish_step_3 - start_step_3).to_i}"
 
-      find(:xpath, ta('performanse:AS:loginPage:subscribe', "//label[@for='subscribe']")).click
-      finish_step_8 = Time.now
-      puts time_step_8 = "time of step 8, seconds: #{(finish_step_8 - finish_step_7).to_i}"
+        sleep 5
+        start_step_4 = Time.now
 
-      find(:xpath, ta('performanse:AS:loginPage:register', "(//button[@class='primary actionSet'])[2]")).click
-      finish_step_9 = Time.now
-      puts time_step_9 = "time of step 9, seconds: #{(finish_step_9 - finish_step_8).to_i}"
+        find(:xpath, ta('AS:loginPage:lastName', "//input[@id='register.lastName']")).set('name')
+        finish_step_4 = Time.now
+        puts time_step_4 = "time of step 4, seconds: #{(finish_step_4 - start_step_4).to_i}"
 
-      sleep 5
-  end
+        sleep 5
+        start_step_5 = Time.now
 
-  end
-end
+        find(:xpath, ta('AS:loginPage:email', "//input[@id='register.email']")).set('name')
+        finish_step_5 = Time.now
+        puts time_step_5 = "time of step 5, seconds: #{(finish_step_5 - start_step_5).to_i}"
+
+        sleep 5
+        start_step_6 = Time.now
+
+        find(:xpath, ta('AS:loginPage:password', "//input[@id='password']")).set('1234tesT')
+        finish_step_6 = Time.now
+        puts time_step_6 = "time of step 6, seconds: #{(finish_step_6 - start_step_6).to_i}"
+
+        sleep 5
+        start_step_7 = Time.now
+
+        find(:xpath, ta('AS:loginPage:checkPwd', "//input[@id='register.checkPwd']")).set('1234tesT')
+        finish_step_7 = Time.now
+        puts time_step_7 = "time of step 7, seconds: #{(finish_step_7 - start_step_7).to_i}"
+
+        sleep 5
+        start_step_8 = Time.now
+
+        find(:xpath, ta('AS:loginPage:subscribe', "//label[@for='subscribe']")).click
+        finish_step_8 = Time.now
+        puts time_step_8 = "time of step 8, seconds: #{(finish_step_8 - start_step_8).to_i}"
+
+        sleep 5
+        start_step_9 = Time.now
+
+        find(:xpath, ta('AS:loginPage:register', "(//button[@class='primary actionSet'])[2]")).click
+        finish_step_9 = Time.now
+        puts time_step_9 = "time of step 9, seconds: #{(finish_step_9 - start_step_9).to_i}"
+
+        sleep 5
+        end
+      end
+      scenario 'Searching - Create account' do
+      1.times do
+        visit 'https://annieselke.com'
+        within_frame(1) do
+            page.execute_script("document.querySelector('button.close').click()")
+        end
+
+        sleep 5
+        start_step_1 = Time.now
+
+        find(:xpath, ta('AS:mainPage:loginDesktop', "//a[text()='Login']")).click
+        finish_step_1 = Time.now
+        puts time_step_1 = "time of step 1, seconds: #{(finish_step_1 - start_step_1).to_i}"
+
+        sleep 5
+        start_step_2 = Time.now
+
+        find(:xpath, ta('AS:mainPage:createAccount', "//h3[text()='Create an Account']")).click
+        finish_step_2 = Time.now
+        puts time_step_2 = "time of step 2, seconds: #{(finish_step_2 - start_step_2).to_i}"
+
+        sleep 5
+        start_step_3 = Time.now
+
+        find(:xpath, ta('AS:loginPage:firstName', "//input[@id='register.firstName']")).set('youre.example@gmail.com')
+        finish_step_3 = Time.now
+        puts time_step_3 = "time of step 3, seconds: #{(finish_step_3 - start_step_3).to_i}"
+
+        sleep 5
+        start_step_4 = Time.now
+
+        find(:xpath, ta('AS:loginPage:lastName', "//input[@id='register.lastName']")).set('name')
+        finish_step_4 = Time.now
+        puts time_step_4 = "time of step 4, seconds: #{(finish_step_4 - start_step_4).to_i}"
+
+        sleep 5
+        start_step_5 = Time.now
+
+        find(:xpath, ta('AS:loginPage:email', "//input[@id='register.email']")).set('name')
+        finish_step_5 = Time.now
+        puts time_step_5 = "time of step 5, seconds: #{(finish_step_5 - start_step_5).to_i}"
+
+        sleep 5
+        start_step_6 = Time.now
+
+        find(:xpath, ta('AS:loginPage:password', "//input[@id='password']")).set('1234tesT')
+        finish_step_6 = Time.now
+        puts time_step_6 = "time of step 6, seconds: #{(finish_step_6 - start_step_6).to_i}"
+
+        sleep 5
+        start_step_7 = Time.now
+
+        find(:xpath, ta('AS:loginPage:checkPwd', "//input[@id='register.checkPwd']")).set('1234tesT')
+        finish_step_7 = Time.now
+        puts time_step_7 = "time of step 7, seconds: #{(finish_step_7 - start_step_7).to_i}"
+
+        sleep 5
+        start_step_8 = Time.now
+
+        find(:xpath, ta('AS:loginPage:subscribe', "//label[@for='subscribe']")).click
+        finish_step_8 = Time.now
+        puts time_step_8 = "time of step 8, seconds: #{(finish_step_8 - start_step_8).to_i}"
+
+        sleep 5
+        start_step_9 = Time.now
+
+        find(:xpath, ta('AS:loginPage:register', "(//button[@class='primary actionSet'])[2]")).click
+        finish_step_9 = Time.now
+        puts time_step_9 = "time of step 9, seconds: #{(finish_step_9 - start_step_9).to_i}"
+
+        sleep 5
+        end
+      end
+   end
