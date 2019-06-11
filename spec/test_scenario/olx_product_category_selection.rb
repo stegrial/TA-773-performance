b8df85ab096548ae5de4c1eff6a8fea1b6e2a700
jarapova <arapova.jul2@gmail.com>
2019-06-11 12:27:47 +0300

    olx_beautyCode

--- spec/test_scenario/olx_product_category_selection.rb
+++ spec/test_scenario/olx_product_category_selection.rb
@@ -1,23 +1,56 @@
-
 require 'spec_helper'
-
-feature 'TrueAutomation.IO capybara example' do
-    scenario 'OLX product category selection' do
-        visit 'https://www.olx.ua/'
-        find(:xpath, "//span[@class='cat-icon cat-icon-circle cat-icon-35']").click
-        find(:xpath, "//a[@data-id='1520']").click
-        find(:xpath, "//a[@class='topTabView link']").click
-        find(:xpath, "//ul[@class='gallerywide clr normal ']/li[12]").click
+describe 'Preconditions' do
+    after(:all) do
+        Capybara.current_session.driver.quit
     end
-
-    scenario 'Searching: OLX product category selection' speed: true do
-
-        2.times do
+    feature 'TrueAutomation.IO capybara example' do
+        scenario 'OLX product category selection' selenium: true do
             # User goes to the main page of the site
             visit 'https://www.olx.ua/'
 
             sleep 5
             start_step_1 = Time.now
+
+            # User clicks on the animal Category to select category
+            find(:xpath, "//span[@class='cat-icon cat-icon-circle cat-icon-35']").click
+            finish_step_1 = Time.now
+            puts time_step_1 = "time of step 1, seconds: #{(finish_step_1 - start_step_1).to_i}"
+
+            sleep 5
+            start_step_2 = Time.now
+
+            # User clicks on free animal mating to select category
+            find(:xpath, "//a[@data-id='1520']").click
+            finish_step_2 = Time.now
+            puts time_step_2 = "time of step 2, seconds: #{(finish_step_2 - start_step_2).to_i}"
+
+            sleep 5
+            start_step_3 = Time.now
+            # User clicks on gallery view to change category
+
+            find(:xpath, "//a[@class='topTabView link']").click
+            finish_step_3 = Time.now
+            puts time_step_3 = "time of step 3, seconds: #{(finish_step_3 - start_step_3).to_i}"
+
+            sleep 5
+            start_step_4 = Time.now
+
+            # User clicks oт the twelfth product from the list
+            find(:xpath, "//ul[@class='gallerywide clr normal ']/li[12]").click
+            finish_step_4 = Time.now
+            puts time_step_4 = "time of step 4, seconds: #{(finish_step_4 - start_step_4).to_i}"
+
+        end
+
+        scenario 'Searching: OLX product category selection' speed: true do
+
+            2.times do
+            # User goes to the main page of the site
+            visit 'https://www.olx.ua/'
+
+            sleep 5
+            start_step_1 = Time.now
+
             # User clicks on the animal Category to select category
             find(:xpath, ta('signature_updating:olx:main_page:animalCategory', "//span[@class='cat-icon cat-icon-circle cat-icon-35']")).click
             finish_step_1 = Time.now
@@ -25,6 +58,7 @@ feature 'TrueAutomation.IO capybara example' do
 
             sleep 5
             start_step_2 = Time.now
+
             # User clicks on free animal mating to select category
             find(:xpath, ta('signature_updating:olx:main_page:free_animal_mating', "//a[@data-id='1520']")).click
             finish_step_2 = Time.now
@@ -32,6 +66,7 @@ feature 'TrueAutomation.IO capybara example' do
 
             sleep 5
             start_step_3 = Time.now
+
             # User clicks on gallery view to change category
             find(:xpath, ta('signature_updating:olx:product_page:gallery_view', "//a[@class='topTabView link']")).click
             finish_step_3 = Time.now
@@ -39,6 +74,7 @@ feature 'TrueAutomation.IO capybara example' do
 
             sleep 5
             start_step_4 = Time.now
+
             # User clicks oт the twelfth product from the list
             find(:xpath, ta('signature_updating:olx:product_page:product', "//ul[@class='gallerywide clr normal ']/li[12]")).click
             finish_step_4 = Time.now
@@ -46,5 +82,5 @@ feature 'TrueAutomation.IO capybara example' do
 
             sleep 3
         end
-    end
+     end
 end
