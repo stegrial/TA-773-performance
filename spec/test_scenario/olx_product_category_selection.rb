dc410b426b4ee051e490baf07c25d911bcf6cfc4
jarapova <arapova.jul2@gmail.com>
2019-06-14 17:04:51 +0300

    olx_fix_test

--- spec/test_scenario/olx_product_category_selection.rb
+++ spec/test_scenario/olx_product_category_selection.rb
@@ -10,19 +10,29 @@ describe 'Preconditions' do
          # User goes to the main page of the site
          visit 'https://www.olx.ua/'
 
+         # User clicks on the animal Category to select category
+         find(:xpath, "//span[@class='cat-icon cat-icon-circle cat-icon-35']").click
+
+         # User clicks on free animal mating to select category
+         find(:xpath, "//a[@data-id='1520']").click
+
+         # User clicks on gallery view to change category
+
+         find(:xpath, "//a[@class='topTabView link']").click
+
          Benchmark.bm(10) do |x|
-           # User clicks on the animal Category to select category
-           x.report('step 1:') { find(:xpath, "//span[@class='cat-icon cat-icon-circle cat-icon-35']").click }
-
-           # User clicks on free animal mating to select category
-           x.report('step 2:') { find(:xpath, "//a[@data-id='1520']").click }
-
-           # User clicks on gallery view to change category
-
-           x.report('step 3:') { find(:xpath, "//a[@class='topTabView link']").click }
 
            # User clicks oт the twelfth product from the list
-           x.report('step 4:') { find(:xpath, "//ul[@class='gallerywide clr normal ']/li[12]").click }
+           x.report('step 1:') { find(:xpath, "//ul[@class='gallerywide clr normal ']/li[12]").click }
+
+           # User clicks a complaint for reporting
+           x.report('step 2:') { find(:xpath, "//a[@id='reportMe']").click }
+
+           # User select a cause of reporting
+           x.report('step 3:') { find(:xpath, "//input[@id='reason-spam']").click }
+
+           # User accept the complaint of reporting
+           x.report('step 4:') { find(:xpath, "//input[@id='report-submit']").click }
 
          end
        end
@@ -31,22 +41,29 @@ describe 'Preconditions' do
            # User goes to the main page of the site
            visit 'https://www.olx.ua/'
 
+           # User clicks on the animal Category to select category
+           find(:xpath, ta('signature_updating:olx:main_page:animalCategory', "//span[@class='cat-icon cat-icon-circle cat-icon-35']")).click
+
+           # User clicks on free animal mating to select category
+           find(:xpath, ta('signature_updating:olx:main_page:free_animal_mating', "//a[@data-id='1520']")).click
+
+           # User clicks on gallery view to change category
+           find(:xpath, ta('signature_updating:olx:product_page:gallery_view', "//a[@class='topTabView link']")).click
+
            Benchmark.bm(10) do |x|
-
-               # User clicks on the animal Category to select category
-               x.report('step 1:') { find(:xpath, ta('signature_updating:olx:main_page:animalCategory', "//span[@class='cat-icon cat-icon-circle cat-icon-35']")).click }
-
-
-               # User clicks on free animal mating to select category
-               x.report('step 2:') { find(:xpath, ta('signature_updating:olx:main_page:free_animal_mating', "//a[@data-id='1520']")).click }
-
-               # User clicks on gallery view to change category
-               x.report('step 3:') { find(:xpath, ta('signature_updating:olx:product_page:gallery_view', "//a[@class='topTabView link']")).click }
 
                p "-------------------------- Analizator -----------------------"
                # User clicks oт the twelfth product from the list
-               x.report('step 4:') { find(:xpath, ta('signature_updating:olx:product_page:product', "//ul[@class='gallerywide clr normal ']/li[12]")).click }
-               p "-------------------------- Analizator -----------------------"
+               x.report('step 1:') { find(:xpath, ta('signature_updating:olx:product_page:product', "//ul[@class='gallerywide clr normal ']/li[12]")).click }
+
+               # User clicks a complaint for reporting
+               x.report('step 2:') { find(:xpath, ta('signature_updating:olx:product_page:complaint', "//a[@id='reportMe']")).click }
+
+               # User select a cause of reporting
+               x.report('step 3:') { find(:xpath, ta('signature_updating:olx:product_page:cause', "//input[@id='reason-spam']")).click }
+
+               # User accept the complaint of reporting
+               x.report('step 4:') { find(:xpath, ta('signature_updating:olx:product_page:accept', "//input[@id='report-submit']")).click }
 
                sleep 3
            end
@@ -55,31 +72,26 @@ describe 'Preconditions' do
 
           2.times do
             # User goes to the main page of the site
-            visit 'https://www.olx.ua/'
+            visit 'https://www.olx.ua/zhivotnye/besplatno-zhivotnye-i-vyazka/?view=galleryWide'
 
             Benchmark.bm(10) do |x|
 
-              # User clicks on the animal Category to select category
-              x.report('step 1:') { find(:xpath, ta('signature_updating:olx:main_page:animalCategory', "//span[@class='cat-icon cat-icon-circle cat-icon-35']")).click }
-              sleep 3
-
-              # User clicks on free animal mating to select category
-              x.report('step 2:') { find(:xpath, ta('signature_updating:olx:main_page:free_animal_mating', "//a[@data-id='1520']")).click }
-              sleep 3
-
-              # User clicks on gallery view to change category
-              x.report('step 3:') { find(:xpath, ta('signature_updating:olx:product_page:gallery_view', "//a[@class='topTabView link']")).click }
-              sleep 5
-
               p "-------------------------- Analizator -----------------------"
               # User clicks oт the twelfth product from the list
-              x.report('step 4:') { find(:xpath, ta('signature_updating:olx:product_page:product', "//ul[@class='gallerywide clr normal ']/li[12]")).click }
+              x.report('step 1:') { find(:xpath, ta('signature_updating:olx:product_page:product', "//ul[@class='gallerywide clr normal ']/li[12]")).click }
+
+              # User clicks a complaint for reporting
+              x.report('step 2:') { find(:xpath, ta('signature_updating:olx:product_page:complaint', "//a[@id='reportMe']")).click }
+
+              # User select a cause of reporting
+              x.report('step 3:') { find(:xpath, ta('signature_updating:olx:product_page:cause', "//input[@id='reason-spam']")).click }
+
+              # User accept the complaint of reporting
+              x.report('step 4:') { find(:xpath, ta('signature_updating:olx:product_page:accept', "//input[@id='report-submit']")).click }
 
               sleep 3
             end
             sleep 5
-
-            Capybara.current_session.driver.quit
 
           end
        end
