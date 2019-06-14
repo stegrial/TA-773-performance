2f1b22f1673c86e27eaa8df203859d03eb8eb112
jarapova <arapova.jul2@gmail.com>
2019-06-14 15:42:48 +0300

    as_fix

--- spec/test_scenario/annieSelke_create_account.rb
+++ spec/test_scenario/annieSelke_create_account.rb
@@ -78,6 +78,7 @@ describe 'Preconditions' do
             # User set email in matching input line
             x.report('step 5:') { find(:xpath, ta('signature_updating:AS:loginPage:email', "//input[@id='register.email']")).set('name') }
 
+            p "-------------------------- Analizator -----------------------"
             # User set pass in matching input line
             x.report('step 6:') { find(:xpath, ta('signature_updating:AS:loginPage:password', "//input[@id='password']")).set('1234tesT') }
 
@@ -95,7 +96,6 @@ describe 'Preconditions' do
         end
       end
       scenario 'Searching - Create account', ta: true do
-        2.times do
 
           visit 'https://annieselke.com'
           within_frame(1) do
@@ -120,6 +120,7 @@ describe 'Preconditions' do
             # User set email in matching input line
             x.report('step 5:') { find(:xpath, ta('signature_updating:AS:loginPage:email', "//input[@id='register.email']")).set('name') }
 
+            p "-------------------------- Analizator -----------------------"
             # User set pass in matching input line
             x.report('step 6:') { find(:xpath, ta('signature_updating:AS:loginPage:password', "//input[@id='password']")).set('1234tesT') }
 
@@ -134,8 +135,47 @@ describe 'Preconditions' do
 
             sleep 5
           end
-          Capybara.current_session.driver.quit
-        end
       end
+    scenario 'Searching 2 - Create account', ta: true do
+
+        visit 'https://annieselke.com'
+        within_frame(1) do
+          page.execute_script("document.querySelector('button.close').click()")
+        end
+
+        Benchmark.bm(10) do |x|
+          p "-------------------------- Analizator -----------------------"
+          # User clicks on login button in main page
+          x.report('step 1:') { find(:xpath, ta('signature_updating:AS:mainPage:loginDesktop', "//a[text()='Login']")).click }
+
+          # User clicks on create account for starting greating new account
+          x.report('step 2:') { find(:xpath, ta('signature_updating:AS:mainPage:createAccount', "//h3[text()='Create an Account']")).click }
+
+          # User set first name in matching input line
+          x.report('step 3:') { find(:xpath, ta('signature_updating:AS:loginPage:firstName', "//input[@id='register.firstName']")).set('youre.example@gmail.com') }
+
+          # User set email in matching input line
+          x.report('step 4:') { find(:xpath, ta('signature_updating:AS:loginPage:lastName', "//input[@id='register.lastName']")).set('name') }
+
+          p "-------------------------- Analizator -----------------------"
+          # User set email in matching input line
+          x.report('step 5:') { find(:xpath, ta('signature_updating:AS:loginPage:email', "//input[@id='register.email']")).set('name') }
+
+          p "-------------------------- Analizator -----------------------"
+          # User set pass in matching input line
+          x.report('step 6:') { find(:xpath, ta('signature_updating:AS:loginPage:password', "//input[@id='password']")).set('1234tesT') }
+
+          # User repeat pass name in matching input line
+          x.report('step 7:') { find(:xpath, ta('signature_updating:AS:loginPage:checkPwd', "//input[@id='register.checkPwd']")).set('1234tesT') }
+
+          # User click register button to complete the registration
+          x.report('step 8:') { find(:xpath, ta('signature_updating:AS:loginPage:subscribe', "//label[@for='subscribe']")).click }
+
+          # User click register button to complete the registration
+          x.report('step 9:') { find(:xpath, ta('signature_updating:AS:loginPage:register', "(//button[@class='primary actionSet'])[2]")).click }
+
+          sleep 5
+        end
+       end
     end
  end
